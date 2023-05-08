55311f9ef9b713ff593a1918c7c641defdcfd0dc
aabettschool <123058171+aabettschool@users.noreply.github.com>
2023-05-08 16:50:08 -0700

    Added stuff

--- main/java/org/example/Main.java
+++ main/java/org/example/Main.java
@@ -5,8 +5,10 @@ public class Main {
 
     }
 
+    //Stuff
     public static double calculateHypotenuse (double a, double b){
-        return Math.sqrt(Math.pow(a,2) + Math.pow(b,2));
+        return Math.sqrt(Math.pow(a,2) + Math.pow(b,2)) ;
+
     }
 
 }
